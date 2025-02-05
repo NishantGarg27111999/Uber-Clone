9ac541fbd7a167a396fefc1a4ea743a60f44a4f0
Nishant Garg <nishantgarg17282@gmail.com>
2025-02-05 20:20:21 +0530

    /logout route done

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,11 +1,17 @@
 const jwt=require('jsonwebtoken');
-
+const blackListTokenModel=require('../models/blackListToken.model');
 const userModel=require('../models/user.model');
 module.exports.authUser=async(req,res,next)=>{
-    const token=req.cookies.token || req.headers.authorization.splid(' ')[1];
+    const token=req.cookies.token || req.headers.authorization?.split(' ')[1];
 
     if(!token){
-        res.status(401).json({message: 'Unauthorized access'});
+        
+        return res.status(401).json({message: 'Unauthorized access'});
+    }
+
+    const blackListed=blackListTokenModel.find({token});
+    if(blackListed){
+        return res.status(401).json({message: 'Unauthorized access'});
     }
     try{
         const decoded=jwt.verify(token,process.env.JWT_SECRET);
@@ -15,6 +21,7 @@ module.exports.authUser=async(req,res,next)=>{
         return next();
     }
     catch(err){
+        console.log("hello");
         return res.status(401).json({message: 'Unauthorized access'});
     }
 
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -16,6 +16,7 @@ router.post('/login',[
 ],userController.loginUser);
 
 router.get('/profile',authMiddleware.authUser,userController.getUserProfile);
+router.get('/logout',userController.logoutUser);
 
 
 module.exports=router;
